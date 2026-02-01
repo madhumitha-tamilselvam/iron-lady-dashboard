034f5f5203f3cdf18b97c0c4b4ebc6447039551e
MADHUMITHA T <126783727+madhumitha-tamilselvam@users.noreply.github.com>
2026-02-01 13:24:32 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  // Simple front-end validation
+
   if(username === "admin" && password === "admin123") {
     window.location.href = "dashboard.html";
   } else {
